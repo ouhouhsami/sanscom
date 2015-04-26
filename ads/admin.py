f75b823dbb157577ae06a4d525e6ef148775a983
ouhouhsami <samuel.goldszmidt@gmail.com>
2015-04-26 19:38:53 +0200

    Admin for moderation
    * Add moderation for Ad/Search in admin
    * Send mail when moderation is done (wether it's set to accepted or refused

--- ads/admin.py
+++ ads/admin.py
@@ -1,32 +1,81 @@
+#-*- coding: utf-8 -*-
 from leaflet.admin import LeafletGeoAdmin
 
+from django import forms
 from django.contrib import admin
+from django.core.mail import EmailMessage
+
 from .models import Ad, AdPicture, Search, HabitationType, AdSearchRelation
 
 
+# ModelForm
+class AdModelForm(forms.ModelForm):
+    moderation_comment = forms.CharField(required=False, widget=forms.Textarea)
+
+    class Meta:
+        model = Ad
+        exclude = ('location', )
+
+
+class SearchModelForm(forms.ModelForm):
+    moderation_comment = forms.CharField(required=False, widget=forms.Textarea)
+
+    class Meta:
+        model = Search
+        exclude = ('location', )
+
+
+# ModelAdmin
 class AdSearchRelationAdmin(admin.ModelAdmin):
     list_display = ['id', 'ad', 'search', 'ad_contacted', 'search_contacted', 'ad_notified', 'search_notified', 'valid']
 
 
-class AdAdmin(LeafletGeoAdmin):
+class BaseAdmin(LeafletGeoAdmin):
+    item_type = None
+
+    def save_model(self, request, obj, form, change):
+        # Override admin save_model
+        # If obj.valid = True => accepted, send mail to user
+        # If obj.valid = False => not accepted, send mail to user
+        if obj.valid is True:
+            # Send mail
+            message = u'''Bonjour,
+            \n\nVotre %s a été validée : %s .
+            \n\nÀ bientôt
+            \n\nL'équipe AcheterSansCom
+            ''' % (self.item_type, obj.full_url)
+            sender = "contact@acheternsanscom.com"
+            recipients = [obj.user.email, ]
+            subject = u"[AcheterSansCom] Votre %s a été validée - %s" % (self.item_type, obj)
+        if obj.valid is False:
+            # Send mail
+            message = u'''Bonjour,
+            \n\nVotre %s a été refusée : %s .
+            \n\nRaison du refus : %s
+            \n\nÀ bientôt
+            \n\nL'équipe AcheterSansCom
+            ''' % (self.item_type, obj.full_url, form.cleaned_data['moderation_comment'])
+            sender = "contact@acheternsanscom.com"
+            recipients = [obj.user.email, ]
+            subject = u"[AcheterSansCom] Votre %s a été refusée - %s" % (self.item_type, obj)
+        if obj.valid is not None:
+            mail = EmailMessage(subject, message, sender, recipients, [sender])
+            mail.send()
+        obj.save(valid=obj.valid)
+
+
+class AdAdmin(BaseAdmin):
     list_display = ['slug', 'price', 'surface', 'transaction']
-    list_filter = ('transaction', )
-
-    def save_model(self, request, obj, form, change):
-        # Override admin save_model
-        # If obj.valid = True => accepted, send mail to user
-        # If obj.valid = False => not accepted, send mail to user
-        obj.save(valid=obj.valid)
+    list_filter = ('transaction', 'valid')
+    form = AdModelForm
+    item_type = "annonce"
 
 
-class SearchAdmin(LeafletGeoAdmin):
+class SearchAdmin(BaseAdmin):
     list_display = ['slug', 'price_max', 'surface_min', 'location']
-
-    def save_model(self, request, obj, form, change):
-        # Override admin save_model
-        # If obj.valid = True => accepted, send mail to user
-        # If obj.valid = False => not accepted, send mail to user
-        obj.save(valid=obj.valid)
+    list_filter = ('transaction', 'valid')
+    form = SearchModelForm
+    item_type = "recherche"
 
 
 admin.site.register(Ad, AdAdmin)
--- ads/models/abstract.py
+++ ads/models/abstract.py
@@ -1,6 +1,7 @@
 #-*- coding: utf-8 -*-
 
 from django.contrib.auth.models import User
+from django.contrib.sites.models import get_current_site
 from django.db import models
 from django.utils.translation import ugettext as _
 
@@ -21,6 +22,10 @@ class BaseModel(TimeStampedModel):
     def save(self, valid=None, *args, **kwargs):
         self.valid = valid
         super(BaseModel, self).save(*args, **kwargs)
+
+    @property
+    def full_url(self):
+        return u''.join(['http://', get_current_site(None).domain, self.get_absolute_url()])
 
     @property
     def sale(self):
